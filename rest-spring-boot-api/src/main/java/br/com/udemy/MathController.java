ef9975d8b83da89f983ae48c319ed86ec27a7d8e
Djeferson Almeida <djefersonalmeida20@gmail.com>
2024-05-14 16:10:03 -0300

    API REST - Exceptions implementadas!

--- rest-spring-boot-api/src/main/java/br/com/udemy/MathController.java
+++ rest-spring-boot-api/src/main/java/br/com/udemy/MathController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.udemy.exceptions.UnsupportedMathOperationException;
+
 @RestController
 public class MathController {
 
@@ -19,16 +21,21 @@ public class MathController {
 	 @PathVariable(value = "numberTwo")String numberTwo
 	  )throws Exception{
 		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) { 
-			throw new Exception();
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
 		}
 	 return convertToDouble(numberOne) + convertToDouble(numberTwo);
 	}
 	
 	private Double convertToDouble(String strNumber) {
-		return null;
+		if (strNumber == null) return 0D;
+		String number = strNumber.replaceAll(".",",");
+		if(isNumeric(number)) return Double.parseDouble(number);
+		return 0D;
 	}
-private boolean isNumeric(String numberOne) {
-	// TODO Auto-generated method stub
-	return false;	
+	
+	private boolean isNumeric(String strNumber) {
+	if (strNumber == null) return false;	
+	String number = strNumber.replaceAll(",", ".");
+	return number.matches("[-+]?[0-9]*//.?[0-9]+");
 	}
 }
