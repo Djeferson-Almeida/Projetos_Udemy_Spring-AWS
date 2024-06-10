1159a938c5d783f24c933fa85fc1d7c699a98af8
Djeferson Almeida <djefersonalmeida20@gmail.com>
2024-06-10 10:33:32 -0300

    Implementação de JpaRepository e classe PersonRepository

--- rest-spring-boot-api/src/main/java/br/com/udemy/model/Person.java
+++ rest-spring-boot-api/src/main/java/br/com/udemy/model/Person.java
@@ -3,6 +3,7 @@ package br.com.udemy.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,9 +20,16 @@ public class Person implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(name = "first_name", nullable = false, length= 80)
 	private String firstName;
+	
+	@Column(name= "last_name", nullable = false, length= 80)
 	private String lastName;
+	
+	@Column(nullable = false, length= 100)
 	private String address;
+	
+	@Column(nullable = false, length= 6)
 	private String gender;
 
 	public Person() {
